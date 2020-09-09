654a3d504d018dc233242c6807f818a74f01eeb3
Dan Miller <dan@dmiller.dev>
2020-09-09 14:09:21 -0400

    remove hello world

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use watchexec::run::watch;
 mod watch;
 
 fn main() {
-    println!("Hello World!");
-
     let my_handler = watch::DevHandler {};
     let w = watch(&my_handler);
     let w = match w {
